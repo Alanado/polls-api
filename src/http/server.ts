3c8e5546d131ad62ac645c378f0d33d1da8592c2
Alan Gabriel <alan360gabriel@example.com>
2024-02-07 19:16:49 -0300

    Criação de tabela poll options

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,26 +1,9 @@
 import fastify from "fastify";
-import { z } from "zod";
-import { PrismaClient } from "prisma/prisma-client";
+import { createPoll } from "../routes/create-poll";
 
 const app = fastify();
 
-const prisma = new PrismaClient();
-
-app.post("/polls", async (request, response) => {
-  const createPollBody = z.object({
-    title: z.string(),
-  });
-
-  const { title } = createPollBody.parse(request.body);
-
-  const poll = await prisma.poll.create({
-    data: {
-      title,
-    },
-  });
-
-  return response.status(201).send(poll);
-});
+app.register(createPoll);
 
 app.listen({ port: 3333 }).then(() => {
   console.log("HTTP server running in port 3333!");

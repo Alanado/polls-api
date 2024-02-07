86e9b64c883b4d175c0b552b42e4d1417996d9aa
Alan Gabriel <alan360gabriel@example.com>
2024-02-06 21:34:16 -0300

    Implementação de funcionalidade de criação de enquete

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,6 +1,26 @@
 import fastify from "fastify";
+import { z } from "zod";
+import { PrismaClient } from "prisma/prisma-client";
 
 const app = fastify();
+
+const prisma = new PrismaClient();
+
+app.post("/polls", async (request, response) => {
+  const createPollBody = z.object({
+    title: z.string(),
+  });
+
+  const { title } = createPollBody.parse(request.body);
+
+  const poll = await prisma.poll.create({
+    data: {
+      title,
+    },
+  });
+
+  return response.status(201).send(poll);
+});
 
 app.listen({ port: 3333 }).then(() => {
   console.log("HTTP server running in port 3333!");
